77533f9faa04ce0c3b638a9f2f992cb0d16acd69
aloml2543 <55018554+aloml2543@users.noreply.github.com>
2022-04-28 10:11:35 +0900

    Initial Commit

---
+++ TaKNU/TaKNUApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaKNUApp.swift
+//  TaKNU
+//
+//  Created by 서명원 on 2022/04/27.
+//
+
+import SwiftUI
+
+@main
+struct TaKNUApp: App {
+    var body: some Scene {
+        WindowGroup {
+            settingView()
+        }
+    }
+}
