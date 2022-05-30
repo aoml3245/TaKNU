d00e5d643fcb59f0033c35d8b55957aa3d872176
aloml2543 <55018554+aloml2543@users.noreply.github.com>
2022-05-30 19:29:50 +0900

    Add files via upload
    update

---
+++ TaKNU/View/settingView.swift
@@ -0,0 +1,50 @@
+//
+//  settingView.swift
+//  TaKNU
+//
+//  Created by 서명원 on 2022/04/27.
+//
+
+import SwiftUI
+
+struct settingView: View {
+    @State var alarmState: Bool = true
+    var body: some View {
+        VStack{
+            List{
+                Toggle(isOn: $alarmState, label: {
+                                Text("알람")
+                            })
+                Section(header: Text("계정 설정"), footer: Text(""), content: {
+                    Text("사용자명")
+                    
+                    Button(action: {
+                        // action
+                    }) {
+                        Text("로그아웃")
+                    }
+                    Button(action: {
+                        // action
+                    }) {
+                        Text("탈퇴")
+                    }
+                })
+            }
+        }
+        .toolbar {
+            ToolbarItem(placement: .principal) {
+                Text("설정")
+                    .font(.system(size: 20, weight: .semibold))
+                    .accessibilityAddTraits(.isHeader)
+            }
+        }
+        
+    }
+}
+
+
+struct settingView_Previews: PreviewProvider {
+    static var previews: some View {
+        settingView()
+    }
+}
